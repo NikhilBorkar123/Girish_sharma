07ec1290293392512831292de4f2594d150e5046
SdwnmtSbjInterns <sdwnmtsbjinterns@gmail.com>
2019-06-18 21:26:22 +0530

    myfirst

---
+++ Aadesh App/app/src/main/java/com/example/abc/girishsharma/MyJourneyActivity.java
@@ -0,0 +1,13 @@
+package com.example.abc.girishsharma;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MyJourneyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment);
+    }
+}
