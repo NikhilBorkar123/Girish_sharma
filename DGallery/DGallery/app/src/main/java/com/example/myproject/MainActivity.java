a9929cf6f533560a92cf39035c1d4621e32a834a
SdwnmtSbjInterns <sdwnmtsbjinterns@gmail.com>
2019-06-19 17:32:41 +0530

    issues myissues selectcategory Dgallery

---
+++ DGallery/DGallery/app/src/main/java/com/example/myproject/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myproject;
+import android.os.Bundle;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
+
