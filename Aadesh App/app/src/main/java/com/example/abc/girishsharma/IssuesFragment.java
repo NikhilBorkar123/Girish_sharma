97db4b49f4f7dfde8ea73139dbfc367211f998c2
SdwnmtSbjInterns <sdwnmtsbjinterns@gmail.com>
2019-06-20 15:35:49 +0530

    with issues in main app integrated

--- Aadesh App/app/src/main/java/com/example/abc/girishsharma/IssuesFragment.java
+++ Aadesh App/app/src/main/java/com/example/abc/girishsharma/IssuesFragment.java
@@ -1,17 +1,53 @@
 package com.example.abc.girishsharma;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class IssuesFragment extends Fragment {
+    CardView card_view;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_issues,container,false);
+        final View view=inflater.inflate(R.layout.fragment_issues,container,false);
+        card_view = (CardView)view.findViewById(R.id.cardView1); // creating a CardView and assigning a value.
+        card_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        card_view = (CardView)view.findViewById(R.id.cardView2); // creating a CardView and assigning a value.
+        card_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        card_view = (CardView)view.findViewById(R.id.cardView3); // creating a CardView and assigning a value.
+        card_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container,new MyIssueFragment()).commit();
+            }
+        });
+        card_view = (CardView)view.findViewById(R.id.cardView4); // creating a CardView and assigning a value.
+        card_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
---
+++ Aadesh App/app/src/main/java/com/example/abc/girishsharma/MyIssueFragment.java
@@ -0,0 +1,17 @@
+package com.example.abc.girishsharma;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyIssueFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my_issues,container,false);
+    }
+}
