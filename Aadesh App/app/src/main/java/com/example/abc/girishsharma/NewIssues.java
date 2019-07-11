d814df9198b541be1d72669d6f41439ceefd7e57
unknown <NikhilBorkar123>
2019-07-11 20:48:10 +0530

    add

--- Aadesh App/app/src/main/java/com/example/abc/girishsharma/NewIssues.java
+++ Aadesh App/app/src/main/java/com/example/abc/girishsharma/NewIssues.java
@@ -1,13 +1,89 @@
 package com.example.abc.girishsharma;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class NewIssues extends AppCompatActivity {
+    CardView c1,c2,c3,c4,c5,c6;
+    TextView head1,head2,head3,head4,head5,head6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_issues);
+
+        c1 = findViewById(R.id.str);
+        c2 = findViewById(R.id.pol);
+        c3 = findViewById(R.id.ew);
+        c4 = findViewById(R.id.pt);
+        c5 = findViewById(R.id.trs);
+        c6 = findViewById(R.id.rfm);
+        head1 = findViewById(R.id.stray);
+        head2 = findViewById(R.id.poll);
+        head3 = findViewById(R.id.ele);
+        head4 = findViewById(R.id.pub);
+        head5 = findViewById(R.id.road);
+        head6 = findViewById(R.id.footpath);
+
+
+        c1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = head1.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), NewIssuesForm.class);
+                intent.putExtra("message", str);
+                startActivity(intent);
+            }
+        });
+        c2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = head2.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), NewIssuesForm.class);
+                intent.putExtra("message", str);
+                startActivity(intent);
+            }
+        });
+        c3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = head3.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), NewIssuesForm.class);
+                intent.putExtra("message", str);
+                startActivity(intent);
+            }
+        });
+        c4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = head4.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), NewIssuesForm.class);
+                intent.putExtra("message", str);
+                startActivity(intent);
+            }
+        });
+        c5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = head5.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), NewIssuesForm.class);
+                intent.putExtra("message", str);
+                startActivity(intent);
+            }
+        });
+        c6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = head6.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), NewIssuesForm.class);
+                intent.putExtra("message", str);
+                startActivity(intent);
+            }
+        });
     }
 }
--- Aadesh App/app/src/main/java/com/example/abc/girishsharma/VolunteerFragment.java
+++ Aadesh App/app/src/main/java/com/example/abc/girishsharma/VolunteerFragment.java
@@ -13,6 +13,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.CursorLoader;
 import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -29,7 +30,12 @@ import com.example.abc.girishsharma.Modal.ApiModelData;
 import com.example.abc.girishsharma.Modal.Response;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 
@@ -37,10 +43,11 @@ import static android.app.Activity.RESULT_OK;
 
 public class VolunteerFragment extends Fragment {
     View view;
-    private String s1, s2, s3, s4, s5, s6, s7, s8, s9, s10,pic;
+    private String image,s1, s2, s3, s4, s5, s6, s7, s8, s9, s10;
     TextInputLayout Fname, Lname, Email, Phone, Adr1, Adr2, City, State, Pincode;
     Button submit;
     Spinner profession;
+    private UserSes userSes;
 
 
     private ImageView imgView;
@@ -62,7 +69,10 @@ public class VolunteerFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_volunteer, container, false);
         findViews();
-        view.findViewById(R.id.subbtn).setOnClickListener(new View.OnClickListener() {
+
+        userSes = new UserSes(getContext());
+
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getData();
@@ -78,14 +88,6 @@ public class VolunteerFragment extends Fragment {
             }
         });
 
-//
-//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.id.image);
-//        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-//        byte[] imageBytes = baos.toByteArray();
-//        String imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT);
-
-
         //set spinner value
         profession = view.findViewById(R.id.spProf);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, spinnerValue);
@@ -96,13 +98,11 @@ public class VolunteerFragment extends Fragment {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view,
                                        int position, long id) {
-                //set selected spinner value here.......
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
                 // TODO Auto-generated method stub
-
             }
         });
 
@@ -117,22 +117,33 @@ public class VolunteerFragment extends Fragment {
         try {
             if (requestCode == 0 && resultCode == RESULT_OK && null != data) {
                 Uri selectedImage = data.getData();
-//                String[] filePathColumn = {MediaStore.Images.Media.DATA};
-//                Cursor cursor = getContext().getContentResolver().query(selectedImage, filePathColumn, null, null, null);
-//                assert cursor != null;
-//                cursor.moveToFirst();
-//                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-//                mediaPath = cursor.getString(columnIndex);
-//                imgView.setImageBitmap(BitmapFactory.decodeFile(mediaPath));
-//                cursor.close();
                 Bitmap bitmapImage = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), selectedImage);
                 imgView.setImageBitmap(bitmapImage);
+
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(selectedImage));
+                Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, 500, 500, false);
+                image = ConvertBitmapToString(resizedBitmap);
+
+
             } else {
                 Toast.makeText(getContext(), "please picked a image", Toast.LENGTH_LONG).show();
             }
         } catch (Exception e) {
             Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_LONG).show();
         }
+    }
+
+    public static String ConvertBitmapToString(Bitmap bitmap){
+        String encodedImage = "";
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        try {
+            encodedImage= URLEncoder.encode(Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return encodedImage;
     }
 
     private void findViews() {
@@ -147,6 +158,7 @@ public class VolunteerFragment extends Fragment {
         City = view.findViewById(R.id.city);
         State = view.findViewById(R.id.state);
         Pincode = view.findViewById(R.id.pincode);
+        submit = view.findViewById(R.id.subbtn);
 
     }
 
@@ -161,14 +173,11 @@ public class VolunteerFragment extends Fragment {
         s8 = City.getEditText().getText().toString();
         s9 = State.getEditText().getText().toString();
         s10 = Pincode.getEditText().getText().toString();
-//        pic = imageString;
-
-
     }
 
     private void sendFormDetails() {
         ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
-        Call<ApiModelData> call = apiInterface.sendDetails("", "", s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, "1","");
+        Call<ApiModelData> call = apiInterface.sendDetails(null,null, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, userSes.getCMSUserAuthenticationID(),image);
         call.enqueue(new Callback<ApiModelData>() {
             @Override
             public void onResponse(Call<ApiModelData> call, retrofit2.Response<ApiModelData> response) {
@@ -178,7 +187,7 @@ public class VolunteerFragment extends Fragment {
                     if (volunteer.getSuccess()) {
                         Toast.makeText(getContext(), "Sumbit data successfully...", Toast.LENGTH_SHORT).show();
                     } else {
-                        Toast.makeText(getContext(), "Something went wrong...", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "server response...", Toast.LENGTH_SHORT).show();
                     }
                 } else {
                     assert volunteer != null;
@@ -189,7 +198,6 @@ public class VolunteerFragment extends Fragment {
 
             @Override
             public void onFailure(Call<ApiModelData> call, Throwable t) {
-
             }
         });
     }
