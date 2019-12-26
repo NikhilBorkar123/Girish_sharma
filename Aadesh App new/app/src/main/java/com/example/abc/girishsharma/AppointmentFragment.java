dd533e9f61504774ad7e8be8fcced091c984d8b6
aadesh1998 <sdwnmtsbjinterns@gmail.com>
2019-12-26 15:12:00 +0530

    share ur details

--- Aadesh App new/app/src/main/java/com/example/abc/girishsharma/AppointmentFragment.java
+++ Aadesh App new/app/src/main/java/com/example/abc/girishsharma/AppointmentFragment.java
@@ -32,150 +32,9 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-<<<<<<< HEAD
 public class AppointmentFragment extends Fragment {
     String pur, nam, ema, prf, pho, ad1, ad2, cit, sta, pin, refn, refp, refph;
     TextInputLayout purpose, name, email, phone, adrs1, adrs2, city, state, pinc, refname, refpost, refphone;
-=======
-public class AppointmentFragment extends Fragment{
-//    EditText editPurpose,editName,editEmail,editPhone,editAddress1,editAddress2,editCity,editState,editPincode,editRefernceName,editReferncePost,editReferncePhone;
-//
-//    private Button editbtn;
-//
-//    private LinearLayout linearLayout;
-//
-//    public static final String Base_URL = "http://iamapp.incubatorsdwnmt.com/api/";
-//
-//    private AwesomeValidation awesomeValidation;
-//
-//    private String proff;
-//
-//    private ApiInterface apiInterface;
-//
-//    View view;
-//    @Nullable
-//    @Override
-//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//        view= inflater.inflate(R.layout.fragment_appointment,container,false);
-//
-//        editbtn = view.findViewById(R.id.editbtn);
-//
-//        linearLayout = view.findViewById(R.id.linear);
-//
-//        awesomeValidation = new AwesomeValidation(ValidationStyle.COLORATION);
-//        findViews();
-//        validateForm();
-//        try {
-//            initSpinner();
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-//        Retrofit retrofit = new Retrofit.Builder()
-//                .baseUrl(Base_URL)
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .build();
-//
-//        apiInterface = retrofit.create(ApiInterface.class);
-//        editbtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(awesomeValidation.validate()) {
-//                    try {
-//                        submitForm();
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//                Log.e("Onclick event","Form submitted successfully|!");
-//            }
-//        });
-//        return view;
-//    }
-//    private void findViews(){
-//        editPurpose =view.findViewById(R.id.editPurpose);
-//        editName =  view.findViewById(R.id.editName);
-//        editEmail =  view.findViewById(R.id.editEmail);
-//        editPhone =  view.findViewById(R.id.editPhone);
-//        editAddress1=  view.findViewById(R.id.editAddress1);
-//        editAddress2=  view.findViewById(R.id.editAddress2);
-//        editCity =  view.findViewById(R.id.editCity);
-//        editState =  view.findViewById(R.id.editState);
-//        editPincode =  view.findViewById(R.id.editPincode);
-//        editRefernceName =  view.findViewById(R.id.editRefernceName);
-//        editReferncePost =  view.findViewById(R.id.editReferncePost);
-//        editReferncePhone =  view.findViewById(R.id.editReferncePhone);
-//
-//    }
-//    private void validateForm(){
-//
-//        awesomeValidation.addValidation(getActivity(),R.id.editPurpose,"^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$", R.string.nameerror);
-//        awesomeValidation.addValidation(getActivity(), R.id.editName, "^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$", R.string.nameerror);
-//        awesomeValidation.addValidation(getActivity(), R.id.editEmail, Patterns.EMAIL_ADDRESS, R.string.nameerror);
-//        awesomeValidation.addValidation(getActivity(), R.id.editPhone, RegexTemplate.TELEPHONE, R.string.nameerror);
-//        awesomeValidation.addValidation(getActivity(), R.id.editCity, "^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$", R.string.nameerror);
-//        awesomeValidation.addValidation(getActivity(), R.id.editState, "^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$", R.string.nameerror);
-//        awesomeValidation.addValidation(getActivity(), R.id.editRefernceName, "^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$", R.string.nameerror);
-//        awesomeValidation.addValidation(getActivity(), R.id.editReferncePost, "^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$", R.string.nameerror);
-//    }
-//
-//    private void initSpinner() throws Exception{
-//        Spinner spinner = view.findViewById(R.id.spinner1);
-//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),R.array.Profession, android.R.layout.simple_spinner_item);
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner.setAdapter(adapter);
-//        spinner.setOnItemSelectedListener(this);
-//
-//    }
-//
-//
-//    private void submitForm() throws Exception {
-//
-//        (apiInterface.postAppointments(editPurpose.getText().toString().trim(),
-//                editName.getText().toString().trim(),
-//                editEmail.getText().toString().trim(),
-//                editPhone.getText().toString().trim(),
-//                editAddress1.getText().toString().trim(),
-//                editAddress2.getText().toString().trim(),
-//                editCity.getText().toString().trim(),
-//                editState.getText().toString().trim(),
-//                editPincode.getText().toString().trim(),
-//                editRefernceName.getText().toString().trim(),
-//                editReferncePost.getText().toString().trim(),
-//                editReferncePhone.getText().toString().trim(),
-//                proff
-//        )).enqueue(new Callback<Appointment>() {
-//            @Override
-//            public void onResponse(Call<Appointment> call, Response<Appointment> response) {
-//                try {
-//                    Call<Appointment> userdetails = call;
-//                    Toast.makeText(getContext(), "Successful", Toast.LENGTH_SHORT).show();
-//                    Log.e("onResponse","Submitted successfully");
-//                }catch (Exception e){
-//                    Log.e("submitFrom",e.toString());
-//                }
-//            }
-//            @Override
-//            public void onFailure(retrofit2.Call<Appointment> call, Throwable t) {
-//                Toast.makeText(getContext(), toString(), Toast.LENGTH_LONG).show();
-//            }
-//        });
-//    }
-//
-//
-//    @Override
-//    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//        proff = parent.getItemAtPosition(position).toString();
-//        Log.e("onItemSelected",proff);
-//    }
-//
-//    @Override
-//    public void onNothingSelected(AdapterView<?> parent) {
-//
-//    }
-//
-    String pur,nam,ema,prf,pho,ad1,ad2,cit,sta,pin,refn,refp,refph;
-    TextInputLayout purpose,name,email,phone,adrs1,adrs2,city,state,pinc,refname,refpost,refphone;
->>>>>>> 13b40d0e80a505aafda781b8b5c38f78b8eb8ad5
     Spinner profession;
 //    private AwesomeValidation awesomeValidation;
 
@@ -196,18 +55,13 @@ public class AppointmentFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-<<<<<<< HEAD
         view = inflater.inflate(R.layout.fragment_appointment, container, false);
-=======
-        view= inflater.inflate(R.layout.fragment_appointment,container,false);
->>>>>>> 13b40d0e80a505aafda781b8b5c38f78b8eb8ad5
 //        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
 //        validateForm();
         findViews();
 //        getApp();
         view.findViewById(R.id.subbtn1).setOnClickListener(new View.OnClickListener() {
             @Override
-<<<<<<< HEAD
             public void onClick(View v) {
 //                getApp();
 //                validateForm();
@@ -218,18 +72,6 @@ public class AppointmentFragment extends Fragment{
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-=======
-            public void onClick(View v){
-//                getApp();
-//                validateForm();
-//                if(awesomeValidation.validate()) {
-                    try {
-//                        getApp();
-                        sendAppDetails();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
->>>>>>> 13b40d0e80a505aafda781b8b5c38f78b8eb8ad5
 //                }
             }
         });
@@ -254,20 +96,6 @@ public class AppointmentFragment extends Fragment{
         });
         return view;
     }
-    private void findViews() {
-        purpose = view.findViewById(R.id.purpose);
-        name = view.findViewById(R.id.name);
-        email = view.findViewById(R.id.email);
-        phone = view.findViewById(R.id.phone);
-        adrs1 = view.findViewById(R.id.Address1);
-        adrs2 = view.findViewById(R.id.Address2);
-        profession = view.findViewById(R.id.spProf);
-        city = view.findViewById(R.id.city);
-        state = view.findViewById(R.id.state);
-        pinc = view.findViewById(R.id.pincode);
-        refname = view.findViewById(R.id.refname);
-        refpost = view.findViewById(R.id.refpost);
-        refphone = view.findViewById(R.id.refphone);
 
     private void findViews() {
         purpose = view.findViewById(R.id.purpose);
@@ -317,11 +145,7 @@ public class AppointmentFragment extends Fragment{
 //        refp = refphone.getEditText().getText().toString();
 //        refph = refpost.getEditText().getText().toString();
 
-<<<<<<< HEAD
     //    }
-=======
-//    }
->>>>>>> 13b40d0e80a505aafda781b8b5c38f78b8eb8ad5
     private void sendAppDetails() {
 
         pur = purpose.getEditText().getText().toString();
@@ -338,7 +162,6 @@ public class AppointmentFragment extends Fragment{
         refp = refphone.getEditText().getText().toString();
         refph = refpost.getEditText().getText().toString();
 
-<<<<<<< HEAD
 
         Log.e("Params check", pur + nam + prf + ema + pho + ad1 + ad2 + cit + sta + pin + refn + refp + refph);
 
@@ -370,37 +193,4 @@ public class AppointmentFragment extends Fragment{
         });
     }
 
-=======
-
-
-        Log.e("Params check",pur+nam+prf+ema+pho+ad1+ad2+cit+sta+pin+refn+refp+refph);
-
-        ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
-        Call<AppointmentData> call = apiInterface.sendAppDetails("10","20",nam,prf,ema,pho,ad1,ad2,cit,sta,pin,pur,refn,refp,refph,"100");
-        call.enqueue(new Callback<AppointmentData>() {
-            @Override
-            public void onResponse(Call<AppointmentData> call, retrofit2.Response<AppointmentData> response) {
-                AppointmentData appointment = response.body();
-                Log.e("Err check",appointment.getSuccess().toString());
-                if (appointment != null) {
-                    if (appointment.getSuccess()) {
-//                        Log.v("yes", volunteer.getData().toString());
-                        Toast.makeText(getContext(), "Sumbit data successfully...", Toast.LENGTH_SHORT).show();
-                    } else {
-//                        Log.v("no", volunteer.getData().toString());
-                        Toast.makeText(getContext(), "Something went wrong in submitting...", Toast.LENGTH_SHORT).show();
-                    }
-                } else{
-//                        assert volunteer != null;
-                    Log.v("Response error", appointment.toString());
-                }
-            }
-            @Override
-            public void onFailure(Call<AppointmentData> call, Throwable t) {
-                Log.e("Error Response",t.toString());
-            }
-        });
-    }
-
->>>>>>> 13b40d0e80a505aafda781b8b5c38f78b8eb8ad5
 }
